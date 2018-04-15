d3a42f8161da84632381dd57524f529f3f420136
myoungsubsim <rop2001@gmail.com>
2018-04-15 22:45:32 +0900

    단순 탐색 메서드 추가

--- ch01/ch01.py
+++ ch01/ch01.py
@@ -18,8 +18,19 @@ def binary_search(list, item):
             low = mid + 1
     return None
 
+def normal_search(list, item):
+    # for i in range(len(list)):
+    #     if list[i] == item:
+    #         return i
+
+    for i, value in enumerate(list):
+        if value == item:
+            return i
+
+    return None
 
 my_list = [1, 3, 5, 7, 9]
 
+print(normal_search(my_list, 3))
 print(binary_search(my_list, 3))
 print(binary_search(my_list, -1))
